5ef37a542634f8695b43603dd3ef6ead130fcad4
juliannottenn@hotmail.com <juliannottenn@hotmail.com>
2017-10-22 18:23:54 +0200

    editted help to work with embeds

--- commands/help.js
+++ commands/help.js
@@ -1,16 +1,27 @@
+const discord = require('discord.js')
+
 module.exports.run = async(bot, message, arguments) => {
     const myCommands = bot.commands;
     const commandNames = myCommands.keyArray();
     const longest = commandNames.reduce((long, str) => Math.max(long, str.length), 0);
     let currentCategory = '';
-    let output = `= Command List =\n`;
+
+    let embed = new discord.RichEmbed()
+        .setTitle(":mailbox_with_mail: Command List")
+        .setAuthor(message.author.username, message.author.avatarURL)
+
+    // let output = `= Command List =\n`;
     const sorted = myCommands.array().sort((p, c) => p.help.name > c.help.name);
     sorted.forEach(c => {
-
-        output += `${bot.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}\n`;
+        embed.addField(`${bot.prefix}${c.help.name}`, `${c.help.description}`)
+            // output += `${bot.prefix}${c.help.name}${' '.repeat(longest - c.help.name.length)} :: ${c.help.description}\n`;
     });
+    embed.setFooter('F1Bot©', bot.user.avatarURL)
+        .setThumbnail(bot.user.avatarURL)
+        .setTimestamp(new Date())
+        .setColor("#ff0000")
     message.reply("Help has been sent :incoming_envelope: ")
-    message.author.send(output, { code: 'asciidoc', split: true });
+    message.author.send(embed);
 }
 
 module.exports.help = {
